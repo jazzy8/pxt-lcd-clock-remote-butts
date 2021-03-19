c0b9f525f10f86b145b9328b760783a55d4a8215
jazzy8 <65190962+jazzy8@users.noreply.github.com>
2021-03-18 19:25:38 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("prev")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("ok")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("next")
+})
+radio.setGroup(72)
